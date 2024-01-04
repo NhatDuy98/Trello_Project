7fd7be889624d0d96ddcff644c26e2a22d7ca6a2
LAPTOP-200369FO\ADMIN <dangnhatduy98@gmail.com>
2024-01-04 17:57:43 +0700

    update: done with get all, create and delete work space

---
+++ v1/schemas/work_space_schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from pydantic.alias_generators import to_camel
+
+class ModelConfig(BaseModel):
+    class Config:
+        from_attributes = True
+        alias_generator = to_camel
+
+class PaginationModel(BaseModel):
+    page: int = Field(None)
+    limit: int = Field(None)
+    totalRows: int = Field(None)
+
+class WorkSpaceModel(ModelConfig):
+    work_space_name: str = Field(..., min_length = 1, max_length = 45, description = "name must have 1 to 45 characters")
+    desciption: str | None = Field(None, max_length = 255, description = "too long")
+
+class WorkSpaceCreate(WorkSpaceModel):
+    pass
+
+class WorkSpace(WorkSpaceModel):
+    id: int = Field(..., gt = 0)
+    user_id: int = Field(..., gt = 0)
+
+
+class WorkSpaceResponse(BaseModel):
+    data: list[WorkSpace] = []
+    pagination: PaginationModel
