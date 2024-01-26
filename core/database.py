735eb15a91ae20955510949544d8b4b0144873b8
duy <dangnhatduy98@gmail.com>
2024-01-26 09:05:50 +0700

    fix pool size database

--- core/database.py
+++ core/database.py
@@ -12,8 +12,8 @@ engine = create_engine(
     settings.DATABASE_URL,
     pool_pre_ping = True,
     pool_recycle = 300,
-    pool_size = 5,
-    max_overflow = 0
+    pool_size = 2,
+    max_overflow = 0,
 )
 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
